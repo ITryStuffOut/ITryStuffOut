52b12d3f0742b88ef12d1689fbcff37ccd7cc837
ITryStuffOut <133933247+ITryStuffOut@users.noreply.github.com>
2023-05-18 09:06:13 -0400

    Add files via upload

---
+++ useAnyBlook.js
@@ -0,0 +1,21 @@
+/**
+* @license StewartPrivateLicense-2.0.1
+* Copyright (c) Aerell McKnight 2023
+*
+* You may not reproduce or distribute any code inside this file without the licenser's permission.
+* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+* You may not under any circumstance republish any code from this file as your own.
+* 
+* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
+* https://github.com/Minesraft2/Blooket-Cheats/blob/main/LICENSE
+*/
+
+/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+
+(async () => {
+    const { stateNode } = Object.values(document.querySelector('body > div > div > div'))[1].children[0]._owner;
+    const blooks = webpackJsonp.push([[], { ['1234']: (_, a, b) => { a.webpack = b } }, [['1234']]]).webpack("MDrD").a;
+    if (location.pathname == "/blooks") stateNode.setState({ blookData: Object.keys(blooks).reduce((a, b) => (a[b] = (stateNode.state.blookData[b] || 1), a), {}), allSets: Object.values(blooks).reduce((a, b) => (a.includes(b.set) ? a : a.concat(b.set)), []) });
+    else if (Array.isArray(stateNode.state.unlocks)) stateNode.setState({ unlocks: Object.keys(blooks) });
+    else stateNode.setState({ unlocks: blooks });
+})();
